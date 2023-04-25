68eb2820ba813b6f888b14720492a72edb3f4718
Hyunwook Jo <acerghjk@gmail.com>
2023-04-25 12:02:30 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from PIL import Image
 import numpy as np
 import torchvision
-import torchvision.transforms as T
+import torchvision.transforms as transforms
 
 # We are using STL (for speed and also since ImageNet is no longer publicly available)
 USING_STL = True
@@ -15,66 +9,65 @@ if USING_STL:
     DatasetSubclass = torchvision.datasets.STL10
 else:
     DatasetSubclass = torchvision.datasets.ImageNet
-    
+
 class Dataset(DatasetSubclass):
-    """
+    '''
     Dataset Class
     Implements a general dataset class for STL10 and ImageNet
-    Valeus:
-        hr_size : spatial size of high-resolution image, a list/tuple
-        lr_size : spatial size of low-resolution image, a list/tuple
+    Values:
+        hr_size: spatial size of high-resolution image, a list/tuple
+        lr_size: spatial size of low-resolution image, a list/tuple
         *args/**kwargs: all other arguments for subclassed torchvision dataset
-    """
-    
+    '''
+
     def __init__(self, *args, **kwargs):
         hr_size = kwargs.pop('hr_size', [96, 96])
         lr_size = kwargs.pop('lr_size', [24, 24])
         super().__init__(*args, **kwargs)
-        
+
         if hr_size is not None and lr_size is not None:
             assert hr_size[0] == 4 * lr_size[0]
             assert hr_size[1] == 4 * lr_size[1]
-            
-        # High -res images are cropped and scaled to [-1, 1]
+
+        # High-res images are cropped and scaled to [-1, 1]
         self.hr_transforms = transforms.Compose([
-            T.RandomCrop(hr_size),
-            T.RandomHorizontalFlip(),
-            T.Lambda(lambda img: np.array(img)),
-            T.ToTensor(),
-            T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.RandomCrop(hr_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.Lambda(lambda img: np.array(img)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
-        
+
         # Low-res images are downsampled with bicubic kernel and scaled to [0, 1]
-        self.lr_transforms = T.Compose([
-            T.Normalize((-1.0, -1.0, -1.0), (2.0, 2.0, 2.0)),
-            T.ToPILImage(),
-            T.Resize(lr_size, interpolation=Image.BICUBIC),
-            T.ToTensor(),
+        self.lr_transforms = transforms.Compose([
+            transforms.Normalize((-1.0, -1.0, -1.0), (2.0, 2.0, 2.0)),
+            transforms.ToPILImage(),
+            transforms.Resize(lr_size, interpolation=Image.BICUBIC),
+            transforms.ToTensor(),
         ])
-        
-        self.to_pil = T.ToPILImage()
-        self.to_tensor = T.ToTensor()
-        
+
+        self.to_pil = transforms.ToPILImage()
+        self.to_tensor = transforms.ToTensor()
+
     def __getitem__(self, idx):
         # Uncomment the following lines if you're using ImageNet
         # path, label = self.imgs[idx]
         # image = Image.open(path).convert('RGB')
-        
+
         # Uncomment the following if you're using STL
-        image = th.from_numpy(self.data[idx])
+        image = torch.from_numpy(self.data[idx])
         image = self.to_pil(image)
-        
+
         hr = self.hr_transforms(image)
         lr = self.lr_transforms(hr)
         return hr, lr
-    
+
     @staticmethod
     def collate_fn(batch):
         hrs, lrs = [], []
-        
+
         for hr, lr in batch:
             hrs.append(hr)
             lrs.append(lr)
-            
-        return th.stack(hrs, dim=0)m th.stack(lrs, dim=0)
 
+        return torch.stack(hrs, dim=0), torch.stack(lrs, dim=0)
