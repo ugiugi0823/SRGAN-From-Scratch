1418ec435bf9e99c91a953ac4ba9f34afab819f0
Hyunwook Jo <acerghjk@gmail.com>
2023-04-25 12:03:56 +0900

    Update train_srg.py

--- train_srg.py
+++ train_srg.py
@@ -1,93 +1,61 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=500):
     generator = generator.to(device).train()
     discriminator = discriminator.to(device).train()
     loss_fn = Loss(device=device)
-    
-    g_optimizer = th.optim.Adam(generator.parameters(), lr=lr)
-    d_optimizer = th.optim.Adam(discriminator.parameters(), lr=lr)
-    g_scheduler = th.optim.lr_scheduler.LambdaLR(g_optimizer, lambda _: 0.1)
-    d_scheduler = th.optim.lr_scheduler.LambdaLR(d_optimizer, lambda _: 0.1)
-    
+
+    g_optimizer = torch.optim.Adam(generator.parameters(), lr=lr)
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr)
+    g_scheduler = torch.optim.lr_scheduler.LambdaLR(g_optimizer, lambda _: 0.1)
+    d_scheduler = torch.optim.lr_scheduler.LambdaLR(d_optimizer, lambda _: 0.1)
+
     lr_step = total_steps // 2
     cur_step = 0
-    
+
     mean_g_loss = 0.0
     mean_d_loss = 0.0
-    
+
     while cur_step < total_steps:
         for hr_real, lr_real in tqdm(dataloader, position=0):
             hr_real = hr_real.to(device)
             lr_real = lr_real.to(device)
-            
-            # Enable autocast to FP16 tensors(new feature sicne torch==1.6.0)
+
+            # Enable autocast to FP16 tensors (new feature since torch==1.6.0)
             # If you're running older versions of torch, comment this out
-            # and use NVIDIA apex for mixed/half percision training
+            # and use NVIDIA apex for mixed/half precision training
             if has_autocast:
-                with th.cuda.amp.autocast(enabled=(device=='cuda')):
-                    g_loss, d_loss, hr_fake = loss.fn(
+                with torch.cuda.amp.autocast(enabled=(device=='cuda')):
+                    g_loss, d_loss, hr_fake = loss_fn(
                         generator, discriminator, hr_real, lr_real,
                     )
             else:
                 g_loss, d_loss, hr_fake = loss_fn(
                     generator, discriminator, hr_real, lr_real,
                 )
-                
+
             g_optimizer.zero_grad()
             g_loss.backward()
             g_optimizer.step()
-            
+
             d_optimizer.zero_grad()
             d_loss.backward()
             d_optimizer.step()
-            
+
             mean_g_loss += g_loss.item() / display_step
             mean_d_loss += d_loss.item() / display_step
-            
+
             if cur_step == lr_step:
                 g_scheduler.step()
                 d_scheduler.step()
                 print('Decayed learning rate by 10x.')
-                
-                
+
             if cur_step % display_step == 0 and cur_step > 0:
                 print('Step {}: Generator loss: {:.5f}, Discriminator loss: {:.5f}'.format(cur_step, mean_g_loss, mean_d_loss))
-                show_tensor_images(lr_real * 2 -1 )
+                show_tensor_images(lr_real * 2 - 1)
                 show_tensor_images(hr_fake.to(hr_real.dtype))
                 show_tensor_images(hr_real)
                 mean_g_loss = 0.0
                 mean_d_loss = 0.0
-                
-                
+
             cur_step += 1
             if cur_step == total_steps:
                 break
-                
-                
-
-
-
-
-generator = th.load('srresnet.pt')
-discriminator = Discriminator(n_blocks=1, base_channels=8)
-
-# Uncomment the following lines if you're using ImageNet
-# train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=500)
-# torch.save(generator, 'srgenerator.pt')
-# torch.save(discriminator, 'srdiscriminator.pt')
-
-# Uncomment the following lines if you're using STL
-
-
-train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=1000)
-th.save(generator, 'srgenerator.pt')
-th.save(discriminator, 'srdiscriminator.pt')
-
-
-        
-
